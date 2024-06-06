4d651e826370cc9e79656c3efd10d27fa0e84824
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2024-06-06 10:53:01 +0530

    added sepearate reds for each input.

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,28 @@
   <Editor 
     size="textfield"
     label="Your thoughts" 
-    v-model="content" 
+    v-model="content1" 
   />
 
   <h1>As a text-area</h1>
   <Editor 
     size="textarea"
     label="Write your message" 
-    v-model="content" 
+    v-model="content2" 
   />
 
   <h1>Required field</h1>
   <Editor  
     required
     label="Write your message" 
-    v-model="content" 
+    v-model="content3" 
   />
 
   <h1>Custom Required message</h1>
   <Editor  
     required
     label="Write your message" 
-    v-model="content" 
+    v-model="content4" 
     required-message="This field is required. please fill it"
   />
 
@@ -34,14 +34,14 @@
     auto-grow
     size="textarea"
     label="Write your message" 
-    v-model="content" 
+    v-model="content5" 
   />
 
   <h1>With Hint</h1>
   <Editor
     hint="A message that is need to be sent."
     label="Write your message" 
-    v-model="content" 
+    v-model="content6" 
   />
 </template>
 
@@ -49,7 +49,12 @@
 import { ref } from "vue";
 import Editor from "./components/CustomEditor/index.vue";
 
-const content = ref('<b>A normal message here...</b>')
+const content1 = ref('<b style="color:blue">A normal message here...</b><h2>A heading content</h2>')
+const content2 = ref('<b style="color:blue">A normal message here...</b><h2>A heading content</h2>')
+const content3 = ref('<b style="color:blue">A normal message here...</b><h2>A heading content</h2>')
+const content4 = ref('<b style="color:blue">A normal message here...</b><h2>A heading content</h2>')
+const content5 = ref('<b style="color:blue">A normal message here...</b><h2>A heading content</h2>')
+const content6 = ref('<b style="color:blue">A normal message here...</b><h2>A heading content</h2>')
 </script>
 
 <style>
